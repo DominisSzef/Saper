698da7341abadaa1bc9e706df159aa3264d3d8ef
Dominik <146719152+DominisSzef@users.noreply.github.com>
2024-12-02 14:34:32 +0100

    init

---
+++ saper4g1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace saper4g1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ saper4g1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace saper4g1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int rozmiar = 10;
+    public MainWindow()
+    {
+        InitializeComponent();
+        for (int i = 0; i < rozmiar; i++)
+        {
+            plansza.RowDefinitions.Add(new RowDefinition());
+            plansza.ColumnDefinitions.Add(new ColumnDefinition());
+        }
+    }
+}
